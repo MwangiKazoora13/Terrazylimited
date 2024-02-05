a35c63fffdeb62e229cc858938bd812e9a99393d
MwangiKazoora13 <152798861+MwangiKazoora13@users.noreply.github.com>
2024-02-05 13:59:05 +0300

    Add files via upload

---
+++ TerrazyScript.js
@@ -0,0 +1,33 @@
+//MENU NAVIGATOR
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", ()=>{
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+addEventListener("click", () =>{
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
+
+//SLIDING ANIMATION
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
